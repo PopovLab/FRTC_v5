7a5ecb2b00a56e4ec89bdba4ce58e4baac648ce6
Gr1nnch <70350701+Gr1nnch@users.noreply.github.com>
2024-06-19 16:13:47 +0300

    chengadd

--- sbr/1_chang_cooper_solver.f90
+++ sbr/1_chang_cooper_solver.f90
@@ -118,6 +118,31 @@ subroutine chang_cooper_abcoef(alfa2, A,B,C,f,Y,dt,n,ybeg,yend,xx,h,df)
 end
 !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 
+subroutine tridag(a,b,c,r,u,n)
+    implicit none
+    integer n,nmax
+    real*8 r(n),u(n)
+    real*8 a(n),b(n),c(n),eps
+    parameter (nmax=1000000,eps=1.d-200)
+    integer j
+    double precision bet,gam(nmax)
 
+
+    if(b(1).eq.0.d0)pause 'tridag: rewrite equations'
+    bet=b(1)
+    u(1)=r(1)/bet
+    do 11 j=2,n
+      gam(j)=c(j-1)/bet
+      bet=b(j)-a(j)*gam(j)
+      if(bet.eq.0.d0)pause 'tridag failed'
+      u(j)=(r(j)-a(j)*u(j-1))/bet
+11    continue
+    do 12 j=n-1,1,-1
+      u(j)=u(j)-gam(j+1)*u(j+1)
+12    continue
+
+    return
+
+    end
 
 end module Chang_Cooper_module

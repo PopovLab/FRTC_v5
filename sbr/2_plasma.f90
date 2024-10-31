a33a97eb0af637cc43418734999ce8144054b954
nteplova <natalia.teplova@mail.ioffe.ru>
2024-10-31 11:01:31 +0300

    gelik N4N6

--- sbr/2_plasma.f90
+++ sbr/2_plasma.f90
@@ -71,6 +71,7 @@ contains
         integer, intent(in)  :: NA1
         real(wp), intent(in) :: ABC, BTOR, RTOR, UPDWN, GP2
         real(wp), dimension(*) :: AMETR, RHO, SHIF, ELON, TRIA,MU,  NE, TE, TI, ZEF, UPL
+        real(wp), dimension(NA1) :: sshift !!! manual Shafran shift
         integer i, k
         integer, parameter :: N  = 501
         real(wp) :: znak_tor, znak_pol, fpol, dfmy
@@ -82,10 +83,15 @@ contains
             allocate(temi(N),zeff(N), afld(N), source=0.0_wp)
             allocate(delta(N),ell(N),gamm(N),amy(N), source=0.0_wp)
         end if
+
+        open(20,file='sshift_gelik.dat')
         do i=1, ngrid
+           ! print *, i, ngrid
+            read(20,*) sshift(i)
             rh(i)=AMETR(i)/ABC
             rha(i)=RHO(i)/ABC  !/ABC instead of /ROC is not a mistake!
-            delta(i)=(SHIF(1)-SHIF(i))/ABC  !FRTC Shafr. shift. defin.
+            delta(i)=(sshift(1)-sshift(i))/ABC  !FRTC Shafr. shift. defin.
+            !delta(i)=(SHIF(1)-SHIF(i))/ABC  !FRTC Shafr. shift. defin.
             ell(i)=ELON(i)
             gamm(i)=rh(i)*TRIA(i)
             con(i)=NE(i)
@@ -95,6 +101,8 @@ contains
         !    afld(i)=UPL(i)/RTOR/GP2 !!variant
  	    afld(i)=0
         end do
+        close(20)
+        !print *, '------'
         rh(ngrid)=1.d0
         rh1=rh(1)          !saving the first ASTRA radial grid element
         rh(1) = 0.0d0         !shifting the first element to zero
@@ -108,6 +116,7 @@ contains
         z0=1.d2*UPDWN              !z-coordinate of the magnetic axis, cm
 
 
+        
     !   spline approximation of plasma profiles         
     !
     !   shift as a function of "minor radius":
